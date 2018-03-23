4b247ee2c6c82232e48b40a75d4661ff554b3695
Brian Lehman <brian@viz.farm>
2018-03-23 13:48:41 -0600

    created an adjustScrolling function

--- scroll.js
+++ scroll.js
@@ -2,14 +2,12 @@ var body = d3.select("body");
 var scroll = d3.select("#scroll-content");
 var win = d3.select(window)
 
-body.on("wheel", function() {
-    scroll.property("scrollTop", +scroll.property("scrollTop") + d3.event.deltaY)
-});
+body.on("wheel", adjustScrolling);
 
-win.on("scroll", function() {
-    scroll.property("scrollTop", +scroll.property("scrollTop") + d3.event.deltaY)
-});
+win.on("scroll", adjustScrolling);
 
-window.addEventListener('scroll', function(e) {
+window.addEventListener('scroll', adjustScrolling);
+
+function adjustScrolling(){
   scroll.property("scrollTop", +scroll.property("scrollTop") + d3.event.deltaY)
-});  
+}
