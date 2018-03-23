bfb779aee1f9bda88b80f623ab4a9ffaedd095a1
Brian Lehman <brian@viz.farm>
2018-03-23 12:48:55 -0600

    added "scroll" event listener to body

--- scroll.js
+++ scroll.js
@@ -4,3 +4,6 @@ var scroll = d3.select("#scroll-content");
 body.on("wheel", function() {
     scroll.property("scrollTop", +scroll.property("scrollTop") + d3.event.deltaY)
 });
+body.on("scroll", function() {
+    scroll.property("scrollTop", +scroll.property("scrollTop") + d3.event.deltaY)
+});
